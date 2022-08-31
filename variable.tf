8e62ae090eeaf7a1da014903a410411d30040d74
Aditya Raman <aditya.raman@gmail.com>
2022-08-31 13:18:32 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "project" { 
+    type = string
+    default = "data-project-dev-314214"
+    description = "This is project id"
+    }
+variable "location" {
+    type = string
+    description = "location"
+    default = "us-central1"
+}
