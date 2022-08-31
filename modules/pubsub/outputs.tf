3d785655740fcbc59909eb50a5f34b80cb440e69
Aditya Raman <aditya.raman@gmail.com>
2022-08-31 13:27:07 +0530

    Add files via upload

---
+++ modules/pubsub/outputs.tf
@@ -0,0 +1,8 @@
+
+#output "instance_name" {
+#  value = "${google_compute_instance.test_server.name}"
+#}
+
+# output "external_ip" {
+#  value = "${google_compute_instance.test_server.network_interface.0.access_config.0.nat_ip}"
+#}
---
+++ modules/pubsub/variables.tf
@@ -0,0 +1,10 @@
+variable "topic_name" {}
+variable "subscription_name" {}
+variable "sa_email" {}
+variable "sa_role" {}
+variable "location" {}
+variable "project_id" {}
+variable "service_url" {}
+
+
+
