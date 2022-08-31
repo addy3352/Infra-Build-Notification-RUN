5f33fbda635ce6a826a9e9984df8ef8935908596
Aditya Raman <aditya.raman@gmail.com>
2022-08-31 13:27:55 +0530

    Delete pubsub directory

--- pubsub/outputs.tf
+++
@@ -1,8 +0,0 @@
-
-#output "instance_name" {
-#  value = "${google_compute_instance.test_server.name}"
-#}
-
-# output "external_ip" {
-#  value = "${google_compute_instance.test_server.network_interface.0.access_config.0.nat_ip}"
-#}
